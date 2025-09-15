8df1936b4cfd766e1f5d81ca2f769c7b28866019
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-15 23:05:41 +0200

    changes database

--- db.js
+++ db.js
@@ -5,25 +5,30 @@ let myDishes = [
         "meal": "Chili-Con-Carne á la Malle",
         "price": 12.75,
         "discription": "scharfes Chili mit Rindfleisch, an Paprika, Zwiebeln, Kidneybohnen und Mais",
+        "amount": 0,
     },
     {
         "meal": "Indischer Steckrüben-Hokkaidokürbis-Eintopf",
         "price": 14.95,
         "discription": "Eintopf von der Steckrübe und dem Hokkaido-Kürbis, aus einem feinen Rinderfonds mit Cocosmilch, Peperoni, Möhren und Lauchzwiebel",
+        "amount": 0,
     },
     {
         "meal": "Bolognese á la Bolzum",
         "price": 13.45,
         "discription": "Bolognese mit Rinderhack, an Tomaten, Möhren, Zwiebeln und Paprika",
+        "amount": 0,
     },
-        {
+    {
         "meal": "Scampi-Knobi-Pfanne",
         "price": 19.95,
         "discription": "Scampi an Knoblauch, Zwiebeln, Peperoni, Rosmarin und Petersilie. Dazu frisches Fladenbrot.",
+        "amount": 0,
     },
-        {
+    {
         "meal": "Bandnudeln an Lachs in Limetten-Dill-Sahnesoße",
         "price": 22.55,
         "discription": "Bandnudeln mit frischen Lachs, an einer feinen Sahnesoße mit Limetten-Dill-Aroma und mit Paprikastreifen, Lauchzwiebel, Peperoni und Zwiebeln",
+        "amount": 0,
     },
 ]

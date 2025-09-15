c021111dc4120281c62d26f80595e77e5ab7e746
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-15 22:32:49 +0200

    create database

---
+++ db.js
@@ -0,0 +1,29 @@
+// DEFINITION OBJECT-Database
+
+let myDishes = [
+    {
+        "meal": "Gericht 1",
+        "price": 17.85,
+        "discription": "blabla",
+    },
+    {
+        "meal": "Gericht 2",
+        "price": 17.85,
+        "discription": "blabla",
+    },
+    {
+        "meal": "Gericht 3",
+        "price": 17.85,
+        "discription": "blabla",
+    },
+        {
+        "meal": "Gericht 4",
+        "price": 17.85,
+        "discription": "blabla",
+    },
+        {
+        "meal": "Gericht 5",
+        "price": 17.85,
+        "discription": "blabla",
+    },
+]

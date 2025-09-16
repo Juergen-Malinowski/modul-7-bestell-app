bed9bc899c6deefc850a997b497e13291dd8f582
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-16 21:38:44 +0200

    add dishes works

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Basis JS-Datei
 
 let dishesAdd = document.getElementById("select_dishes");
-dishesAdd.innerHTML = "";
+dishesAdd = "";
+// let dishesAddMarker = []; 
 
 function initOnload() {
     nextDishes = document.getElementById("show_dishes");
@@ -10,15 +11,28 @@ function initOnload() {
     for (let index = 0; index < myDishes.length; index++) {
         nextDishes.innerHTML += renderDishes(index);
     }
+    shoppingBasket.innerHTML += renderShoppingBasket();
 }
 
 function addDishes(index) {
     myDishes[index].amount = myDishes[index].amount + 1;
+    console.log(myDishes[index].amount);
+    // dishesAddMarker += myDishes[index].meal;
+
     switch (myDishes[index].amount - 1) {
         case 0:
-            dishesAdd.innerHTML += renderAddDishes(index);
+            console.log("Case = 0 eingetreten");
+
+            dishesAdd.innerHTML = renderAddDishes(index);
+            console.log(myDishes[index].meal);
+            
             break;        
         default:
             break;
     }
+
+            console.log(dishesAdd);
+
+
+    // dishesAdd += renderAddDishes(index);
 }

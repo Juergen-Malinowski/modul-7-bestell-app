81088da1eb2946df63fde46f83158ff22fbeb487
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-16 22:35:09 +0200

    changes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Basis JS-Datei
 
 let dishesAdd = document.getElementById("select_dishes");
-dishesAdd = "";
-// let dishesAddMarker = []; 
+dishesAdd.innerHTML = "";
 
 function initOnload() {
     nextDishes = document.getElementById("show_dishes");
@@ -11,28 +10,15 @@ function initOnload() {
     for (let index = 0; index < myDishes.length; index++) {
         nextDishes.innerHTML += renderDishes(index);
     }
-    shoppingBasket.innerHTML += renderShoppingBasket();
 }
 
 function addDishes(index) {
     myDishes[index].amount = myDishes[index].amount + 1;
-    console.log(myDishes[index].amount);
-    // dishesAddMarker += myDishes[index].meal;
-
     switch (myDishes[index].amount - 1) {
         case 0:
-            console.log("Case = 0 eingetreten");
-
-            dishesAdd.innerHTML = renderAddDishes(index);
-            console.log(myDishes[index].meal);
-            
+            dishesAdd.innerHTML += renderAddDishes(index);
             break;        
         default:
             break;
     }
-
-            console.log(dishesAdd);
-
-
-    // dishesAdd += renderAddDishes(index);
 }
--- template.js
+++ template.js
@@ -20,33 +20,26 @@ function renderDishes(index) {
     `
 }
 
-function renderShoppingBasket() {
-    return `
-        <h2>Ihr Warenkorb ...</h2>
-
-        <!-- BOX show selected dishes  -->
-        <div class="selection_grafik" id="select_dishes"></div>
-
-        <div class="price_box">
-            <span class="basket_subtotal">Zwischensumme:</span>
-            <span class="basket_subtotal">0,00 €</span>
-        </div>
-
-        <div class="price_box">
-            <span class="basket_subtotal">Fahrtkosten:</span>
-            <span class="basket_subtotal">0,00 €</span>
-        </div>
-
-        <div class="price_box">
-            <span class="basket_total">Gesamtpreis:</span>
-            <span class="basket_total">0,00 €</span>
-        </div>        
-    `
-}
 
 function renderAddDishes(index) {
-    return`
+    return `
         <p class="basket_meal">${myDishes[index].meal}</p>
-        <p class="basket_meal">ICH war in renderAddDishes</p>        
+
+    <div class="icons_position"> 
+
+        <div class="icons_disply_row">  
+            <img class="icon_grafik" src="./assets/Logo/plus_button.png" 
+                onclick="addDishes(${index})" alt="plus-button">
+            <p class="icon_grafik">${myDishes[index].amount}x</p>
+            <img class="icon_grafik" src="./assets/Logo/minus_button.png" 
+                onclick="addDishes(${index})" alt="plus-button"> 
+        </div>             
+        
+        <div class="icons_disply_row_rubbish">   
+            <p class="icon_grafik">${myDishes[index].amount*myDishes[index].price}€</p>
+            <img class="icon_grafik_rubbish" src="./assets/Logo/icons8-mülleimer-50.png" 
+                onclick="addDishes(${index})" alt="plus-button">             
+        </div>           
+    </div>  
     `
 }

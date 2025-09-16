caa72542405ea16c4df4186919537354eadc962e
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-16 11:32:50 +0200

    changes

--- script.js
+++ script.js
@@ -9,12 +9,3 @@ function initOnload() {
 
 }
 
-function renderDishes(index) {
-    return `
-        <div class="single_dishes">
-            <p class="single_dishes_meal">${myDishes[index].meal}</p>
-            <p class="single_dishes_discription">${myDishes[index].discription}</p>
-            <p class="single_dishes_price">${myDishes[index].price} €</p>
-        </div>
-    `
-}
--- template.js
+++ template.js
@@ -1,1 +1,11 @@
 // for JS HTML-Templates
+
+function renderDishes(index) {
+    return `
+        <div class="single_dishes">
+            <p class="single_dishes_meal">${myDishes[index].meal}</p>
+            <p class="single_dishes_discription">${myDishes[index].discription}</p>
+            <p class="single_dishes_price">${myDishes[index].price} €</p>
+        </div>
+    `
+}

ea1fb52d8423e439e07645d2780528fd9f7e6925
Jürgen Malinowski <jm-malle@gmx.de>
2025-09-16 21:38:06 +0200

    add dishes goes

--- template.js
+++ template.js
@@ -20,33 +20,9 @@ function renderDishes(index) {
     `
 }
 
-function renderShoppingBasket() {
-    return `
-        <h2>Ihr Warenkorb ...</h2>
-
-        <!-- BOX show selected dishes  -->
-        <div class="selection_grafik" id="select_dishes"></div>
-
-        <div class="price_box">
-            <span class="basket_subtotal">Zwischensumme:</span>
-            <span class="basket_subtotal">0,00 €</span>
-        </div>
-
-        <div class="price_box">
-            <span class="basket_subtotal">Fahrtkosten:</span>
-            <span class="basket_subtotal">0,00 €</span>
-        </div>
-
-        <div class="price_box">
-            <span class="basket_total">Gesamtpreis:</span>
-            <span class="basket_total">0,00 €</span>
-        </div>        
-    `
-}
 
 function renderAddDishes(index) {
     return`
-        <p class="basket_meal">${myDishes[index].meal}</p>
-        <p class="basket_meal">ICH war in renderAddDishes</p>        
+        <p class="basket_meal">${myDishes[index].meal}</p>    
     `
 }
